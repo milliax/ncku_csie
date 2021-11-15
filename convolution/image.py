f82b35e64abf417d0fc65b6bc96e11b0b08a4d2d
milliax <wei.sivir.pw>
2021-11-15 12:13:22 +0800

    update

--- convolution/image.py
+++ convolution/image.py
@@ -12,16 +12,28 @@ if image is None:
     sys.exit()
 
 #縮小圖片到較適當尺寸。
-image = imutils.resize(image, height=450)
+#image = imutils.resize(image, height=450)
 # 設定kernel size為5x5
 kernel_size = 5
 # 使用numpy建立 5*5且值為1/(5**2)的矩陣作為kernel。
-kernel = np.ones((kernel_size, kernel_size), dtype=np.float32) / kernel_size**2
+#kernel = np.ones((kernel_size, kernel_size), dtype=np.float32) / kernel_size**2
+kernel = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]])
+
+kernel2 = np.array((
+        [-1, -2, -1],
+        [0, 0, 0],
+        [1, 2, 1]))
+
 # 顯示矩陣內容，所有值皆為0.04的5x5矩陣
-print (kernel)
+print(kernel)
 
 # 使用cv2.filter2D進行convolute，
-result = cv2.filter2D(image, dst=-1, kernel=kernel, anchor=(-1, -1), delta=0, borderType=cv2.BORDER_DEFAULT)
+result = cv2.filter2D(image, -1, kernel)
+b_sobel = cv2.filter2D(image,-1,kernel2)
+
+print("showing the picture")
 cv2.imshow("Filter", result)
 cv2.imshow("Original", image)
+# cv2.imwrite("sobel.jpg",b_sobel)
+cv2.imshow("sobel",b_sobel)
 cv2.waitKey(0)
---
+++ convolution/readme.md
@@ -0,0 +1,9 @@
+# Image
+
+## prerequisites
+
+```bash
+pip install opencv-python
+pip install numpy
+pip install imutils
+```
